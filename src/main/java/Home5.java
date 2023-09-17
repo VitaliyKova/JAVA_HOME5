ba0c966566b8bc15d57db2c218e85226d84c1e0d
«Vitaliy.K <dr.kovalyk2017@yandex.ru>
2023-09-17 18:24:34 +0300

    Initial commit

---
+++ src/main/java/Home5.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class Home5 {
+    public static void addContacts(String key, String value, HashMap<String,ArrayList<String>> hashMap) {
+        if(hashMap.containsKey(key)){
+            hashMap.get(key).add(value);
+        }
+        else{
+            ArrayList<String> list = new ArrayList<>();
+            list.add(value);
+            hashMap.put(key,list);
+        }
+    }
+    public static void PrintSortContact(HashMap<String, ArrayList<String>> hashMap){
+        ArrayList <Integer> sort = new ArrayList<>();
+        for (ArrayList<String> item: hashMap.values()) {
+            sort.add(item.size());
+        }
+        sort.sort(Comparator.reverseOrder());
+        for (int i = 0; i < sort.size(); i++) {
+            for (Map.Entry<String, ArrayList<String>> entry : hashMap.entrySet()) {
+                if(sort.get(i) == entry.getValue().size()){
+                    System.out.printf("Имя: %s \n Телефоны: %s \n",entry.getKey(),entry.getValue());
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        HashMap<String,ArrayList<String>> phone_book = new HashMap<>();
+        Scanner in = new Scanner(System.in);
+        Boolean proces = true;
+        String name = "";
+        String phone = "";
+        while (proces == true) {
+            System.out.println("Введите Имя -> ");
+            name = in.nextLine();
+            System.out.println("Введите номер -> ");
+            phone = in.nextLine();
+            addContacts(name,phone,phone_book);
+            System.out.println("Чтобы добавить еще контакт, нажмите ENTER.\n Чтобы выйти введите \"end\"");
+            name = in.nextLine();
+            if(name.equalsIgnoreCase("end")) proces = false;
+        }
+        in.close();
+        PrintSortContact(phone_book);
+    }
+}
